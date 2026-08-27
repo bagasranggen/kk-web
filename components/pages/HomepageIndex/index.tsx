691f87ae94064c31643d134ea7d0e82b6352c34d
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-08-27 17:09:25 +0700

    build: add storybook

---
+++ components/pages/HomepageIndex/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export type HomepageIndexProps = {};
+
+const HomepageIndex = ({}: HomepageIndexProps): React.ReactElement => {
+    return <h1>Hello World!</h1>;
+};
+
+export default HomepageIndex;
