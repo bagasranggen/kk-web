75cb590c15a3b1659777ba0ccb68dc14446dddfa
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-08-28 11:54:49 +0700

    refactor: add button block

--- components/common/Button/Block/index.tsx
+++ components/common/Button/Block/index.tsx
@@ -1,30 +1,73 @@
+'use client';
+
 import React from 'react';
 
 import { ArrayStringProps } from '@/libs/@types';
-import { joinArrayString } from '@/libs/utils';
+import { joinArrayString, mergeRefs } from '@/libs/utils';
+import { useIsTouch } from '@/libs/hooks';
+
+import { useMeasure, useWindowSize } from 'react-use';
 
 import Base, { BaseProps } from '@/components/common/Button/Base';
-import Icon from '@/components/common/Icon';
+import Icon, { ArrowProps } from '@/components/common/Icon';
 
-export type BlockProps = {} & BaseProps;
+export type BlockProps = {
+    color?: NonNullable<ArrowProps['color']> | 'primary' | 'accent';
+    active?: boolean;
+} & (BaseProps & Pick<ArrowProps, 'size'>);
 
-const Block = ({ className, children, ...props }: BlockProps): React.ReactElement => {
+const Block = ({
+    className,
+    children,
+    color = 'dark',
+    size,
+    active,
+    ref,
+    style: styleProps,
+    ...props
+}: BlockProps): React.ReactElement => {
+    const isTouch = useIsTouch();
+    const { width: windowWidth } = useWindowSize();
+    const hasAnimation = !isTouch && windowWidth > 992 && !active;
+    const [buttonRef, { width: buttonWidth }] = useMeasure();
+    const [iconRef, { width: iconWidth }] = useMeasure();
+
     let btnClass: ArrayStringProps = ['btn btn--block'];
+    if (color === 'dark') btnClass.push('btn--dark');
+    if (color === 'light') btnClass.push('btn--light');
+    if (color === 'primary') btnClass.push('btn--primary');
+    if (color === 'accent') btnClass.push('btn--accent');
+    if (size === 'lg') btnClass.push('btn--lg');
+    if (active) btnClass.push('btn--active');
+    if (hasAnimation) btnClass.push('btn--animate');
     if (className) btnClass.push(className);
     btnClass = joinArrayString(btnClass);
 
-    return (
-        <>
-            <Base
-                className={btnClass}
-                {...props}>
-                {children}
+    let style: React.CSSProperties | undefined = undefined;
+    if (hasAnimation && iconWidth > 0 && buttonWidth > 0) {
+        style = Object.assign(style ?? {}, {
+            '--btn-block-scale': (buttonWidth * 2) / iconWidth,
+        } as React.CSSProperties);
+    }
+    if (styleProps) style = Object.assign(style ?? {}, styleProps);
 
-                <div className='btn__icon'>
-                    <Icon.Arrow />
-                </div>
-            </Base>
-        </>
+    return (
+        <Base
+            className={btnClass}
+            ref={mergeRefs(buttonRef, ref)}
+            style={style}
+            {...props}>
+            <span className="btn__text">{children}</span>
+
+            <div
+                ref={iconRef as React.Ref<HTMLDivElement>}
+                className="btn__icon">
+                <Icon.Arrow
+                    color={color === 'light' ? 'dark' : 'light'}
+                    size={size}
+                />
+            </div>
+        </Base>
     );
 };
 
--- components/common/Button/index.stories.tsx
+++ components/common/Button/index.stories.tsx
@@ -17,7 +17,7 @@ const meta = {
     // More on argTypes: https://storybook.js.org/docs/api/arg-types
     argTypes: {},
     // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
-    args: { onClick: fn() },
+    // args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -32,11 +32,137 @@ export const Default: Story = {
     },
 };
 
-export const Block: StoryObj<typeof Button.Block> = {
+type BlockStory = StoryObj<typeof Button.Block>;
+
+const blockArgsTypes: Meta<typeof Button.Block>['argTypes'] = {
+    color: {
+        options: ['light', 'dark'],
+        control: { type: 'select' },
+    },
+    size: {
+        options: ['md', 'lg'],
+        control: { type: 'select' },
+    },
+    active: {
+        control: { type: 'boolean' },
+    },
+};
+
+export const Block: BlockStory = {
+    argTypes: blockArgsTypes,
     args: {
         as: 'anchor',
         children: 'Block Anchor',
         href: '#',
+    },
+    render: (args) => {
+        return <Button.Block {...args} />;
+    },
+};
+export const BlockLarge: BlockStory = {
+    argTypes: blockArgsTypes,
+    args: {
+        as: 'anchor',
+        children: 'Block Anchor',
+        href: '#',
+        size: 'lg',
+    },
+    render: (args) => {
+        return <Button.Block {...args} />;
+    },
+};
+
+export const BlockActive: BlockStory = {
+    argTypes: blockArgsTypes,
+    args: {
+        as: 'anchor',
+        children: 'Block Anchor',
+        href: '#',
+        active: true,
+    },
+    render: (args) => {
+        return <Button.Block {...args} />;
+    },
+};
+
+export const BlockLight: BlockStory = {
+    argTypes: blockArgsTypes,
+    args: {
+        as: 'anchor',
+        children: 'Block Anchor',
+        href: '#',
+        color: 'light',
+        active: false,
+    },
+    render: (args) => {
+        return <Button.Block {...args} />;
+    },
+};
+
+export const BlockLightActive: BlockStory = {
+    argTypes: blockArgsTypes,
+    args: {
+        as: 'anchor',
+        children: 'Block Anchor',
+        href: '#',
+        color: 'light',
+        active: true,
+    },
+    render: (args) => {
+        return <Button.Block {...args} />;
+    },
+};
+
+export const BlockPrimary: BlockStory = {
+    argTypes: blockArgsTypes,
+    args: {
+        as: 'anchor',
+        children: 'Block Anchor',
+        href: '#',
+        color: 'primary',
+        active: false,
+    },
+    render: (args) => {
+        return <Button.Block {...args} />;
+    },
+};
+
+export const BlockPrimaryActive: BlockStory = {
+    argTypes: blockArgsTypes,
+    args: {
+        as: 'anchor',
+        children: 'Block Anchor',
+        href: '#',
+        color: 'primary',
+        active: true,
+    },
+    render: (args) => {
+        return <Button.Block {...args} />;
+    },
+};
+
+export const BlockAccent: BlockStory = {
+    argTypes: blockArgsTypes,
+    args: {
+        as: 'anchor',
+        children: 'Block Anchor',
+        href: '#',
+        color: 'accent',
+        active: false,
+    },
+    render: (args) => {
+        return <Button.Block {...args} />;
+    },
+};
+
+export const BlockAccentActive: BlockStory = {
+    argTypes: blockArgsTypes,
+    args: {
+        as: 'anchor',
+        children: 'Block Anchor',
+        href: '#',
+        color: 'accent',
+        active: true,
     },
     render: (args) => {
         return <Button.Block {...args} />;
