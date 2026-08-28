02aa3f167af9105747c90d6972b3027aa3368831
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-08-28 16:31:12 +0700

    wip: button hamburger

---
+++ components/common/Button/Hamburger/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { ArrayStringProps } from '@/libs/@types';
+import { joinArrayString } from '@/libs/utils';
+
+import Base, { BaseProps } from '@/components/common/Button/Base';
+
+export type HamburgerProps = {
+    active?: boolean;
+} & BaseProps;
+
+const Hamburger = ({ className, active, ...props }: HamburgerProps): React.ReactElement => {
+    let btnClass: ArrayStringProps = ['btn btn--hamburger'];
+    if (active) btnClass.push('btn--active');
+    if (className) btnClass.push(className);
+    btnClass = joinArrayString(btnClass);
+
+    return (
+        <Base
+            className={btnClass}
+            {...props}>
+            <div className="btn__icon" />
+            {/*<div className="btn__icon btn__icon--helper" />*/}
+            {/*<div className="btn__icon btn__icon--x-helper" />*/}
+            <div className="btn__icon" />
+            {/*<div className="btn__icon btn__icon--helper" />*/}
+            {/*<div className="btn__icon btn__icon--x-helper" />*/}
+            <div className="btn__icon" />
+        </Base>
+    );
+};
+
+export default Hamburger;
