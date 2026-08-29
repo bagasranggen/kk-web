4ec49e3ca7be2aa43665b85e24cdb8cc346b36a9
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-08-29 12:17:02 +0700

    refactor: update hamburger button

--- components/common/Button/index.ts
+++ components/common/Button/index.ts
@@ -3,14 +3,17 @@ import { Component } from '@/libs/@types';
 import Base, { BaseProps } from '@/components/common/Button/Base';
 import Arrow, { ArrowProps } from '@/components/common/Button/Arrow';
 import Block, { BlockProps } from '@/components/common/Button/Block';
+import Hamburger, { HamburgerProps } from '@/components/common/Button/Hamburger';
 
 export type * from '@/components/common/Button/Base';
 export type * from '@/components/common/Button/Arrow';
 export type * from '@/components/common/Button/Block';
+export type * from '@/components/common/Button/Hamburger';
 
 type ButtonComposition = {
     Arrow: Component<ArrowProps>;
     Block: Component<BlockProps>;
+    Hamburger: Component<HamburgerProps>;
 };
 
-export default Object.assign<Component<BaseProps>, ButtonComposition>(Base, { Arrow, Block });
+export default Object.assign<Component<BaseProps>, ButtonComposition>(Base, { Arrow, Block, Hamburger });
