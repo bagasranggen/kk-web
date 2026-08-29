bbe4a91a06d03c5488a4f764a4fe259c42c75ccd
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-08-29 12:18:46 +0700

    refactor: add icon logo component

--- components/common/Icon/index.ts
+++ components/common/Icon/index.ts
@@ -1,11 +1,14 @@
 import { Component } from '@/libs/@types';
 
 import Arrow, { ArrowProps } from '@/components/common/Icon/Arrow';
+import Logo, { LogoProps } from '@/components/common/Icon/Logo';
 
 export type * from '@/components/common/Icon/Arrow';
+export type * from '@/components/common/Icon/Logo';
 
 type IconComposition = {
     Arrow: Component<ArrowProps>;
+    Logo: Component<LogoProps>;
 };
 
-export default Object.assign<{}, IconComposition>({}, { Arrow });
+export default Object.assign<{}, IconComposition>({}, { Arrow, Logo });
