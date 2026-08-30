d47cf753d3fa7a6c8769b947c4db0bbd1b7a040f
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-08-30 13:35:59 +0700

    refactor: add apple music icon

--- components/common/Icon/AppleMusic/index.stories.tsx
+++ components/common/Icon/AppleMusic/index.stories.tsx
@@ -31,14 +31,11 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 
 export const Default: Story = {
-    args: {
-        // className: 'max-w-50',
-    },
+    args: {},
 };
 
 export const Dark: Story = {
     args: {
-        // className: 'max-w-50',
         color: 'dark',
     },
 };
