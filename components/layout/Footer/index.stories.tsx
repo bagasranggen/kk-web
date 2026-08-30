96dd9480e4893e48e5f20e813c2e56543c169a56
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-08-30 13:36:40 +0700

    refactor: add social media on footer

--- components/layout/Footer/index.stories.tsx
+++ components/layout/Footer/index.stories.tsx
@@ -3,6 +3,7 @@ import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 import { fn } from 'storybook/test';
 
 import Footer from './index';
+import { LIST_SOCIAL_MEDIA } from '@/components/common/List/SocialMedia/index.mock';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -17,7 +18,7 @@ const meta = {
     // More on argTypes: https://storybook.js.org/docs/api/arg-types
     argTypes: {},
     // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
-    args: { onClick: fn() },
+    // args: { onClick: fn() },
 } satisfies Meta<typeof Footer>;
 
 export default meta;
@@ -25,5 +26,7 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
-    args: {},
+    args: {
+        socialMedia: LIST_SOCIAL_MEDIA,
+    },
 };
--- components/layout/Footer/index.tsx
+++ components/layout/Footer/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import Container from '@/components/common/Container';
 import Icon from '@/components/common/Icon';
+import List, { SocialMediaProps } from '@/components/common/List';
 
-export type FooterProps = {};
+export type FooterProps = {
+    socialMedia?: SocialMediaProps['items'];
+};
 
-const Footer = ({}: FooterProps): React.ReactElement => {
+const Footer = ({ socialMedia }: FooterProps): React.ReactElement => {
     return (
         <footer className="bg-kk-primary pt-8 pb-6">
             <Container>
                 <Icon.Logo
                     color="light"
-                    className="max-w-22 mx-auto"
+                    className="max-w-35 mx-auto"
+                />
+
+                <List.SocialMedia
+                    className="mt-2 justify-center"
+                    color="light"
+                    size="sm"
+                    items={socialMedia}
                 />
             </Container>
         </footer>
