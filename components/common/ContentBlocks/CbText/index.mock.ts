6b0f67cb4a43c660538c4f72e8ca279f2a739e8f
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-09-03 11:54:02 +0700

    refactor: add cb text mock and stories

---
+++ components/common/ContentBlocks/CbText/index.mock.ts
@@ -0,0 +1,18 @@
+import parse from 'html-react-parser';
+
+export const CB_TEXT_CHILDREN = parse(`
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consequuntur eaque iste mollitia, quae quia
+        quo rem. A accusamus asperiores cum cupiditate dicta dolore doloribus ducimus in molestias, porro sed!
+    </p>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid facilis, incidunt magni reprehenderit
+        repudiandae sequi voluptate. Ab accusamus accusantium, alias asperiores at commodi, corporis culpa,
+        deserunt dicta eveniet explicabo facilis fugit illo ipsam laboriosam molestias necessitatibus nesciunt
+        numquam perspiciatis porro quod quos ratione sequi sit sunt suscipit unde ut? Accusamus accusantium
+        consequatur consequuntur, dolores ea enim est exercitationem, fugiat inventore itaque labore obcaecati
+        quae ratione tempora tempore unde ut? Ducimus illum incidunt laboriosam maiores maxime quaerat quasi
+        quis quisquam, similique!
+    </p>
+`);
