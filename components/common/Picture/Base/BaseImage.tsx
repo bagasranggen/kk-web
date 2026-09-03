5dbb86459d3a84abc987ef994eef8c6ece4710a5
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-09-03 11:54:42 +0700

    refactor: render sizes props on image tag

--- components/common/Picture/Base/BaseImage.tsx
+++ components/common/Picture/Base/BaseImage.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 
 export type BaseImageProps = React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>;
 
-const BaseImage = ({ className, src, srcSet, width, height, loading, alt }: BaseImageProps): React.ReactElement => {
+const BaseImage = ({
+    className,
+    src,
+    srcSet,
+    width,
+    height,
+    loading,
+    sizes,
+    alt,
+}: BaseImageProps): React.ReactElement => {
     const props: React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> = {
         src,
         srcSet,
         width,
         height,
         loading,
+        sizes,
         className,
     };
 
