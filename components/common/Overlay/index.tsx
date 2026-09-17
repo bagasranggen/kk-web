607e0936315536272f3a8183baead75128904118
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-09-17 16:07:20 +0700

    refactor: add overlay component

---
+++ components/common/Overlay/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { ArrayStringProps, CreateArrayWithLengthX, NumericRange } from '@/libs/@types';
+import { joinArrayString } from '@/libs/utils';
+
+export type OverlayProps = {
+    variant: 'solid' | 'gradient-left' | 'gradient-right' | 'gradient-top' | 'gradient-bottom';
+    opacity: NumericRange<CreateArrayWithLengthX<0>, 6>;
+    children: React.ReactElement;
+};
+
+const Overlay = ({ variant, opacity, children }: OverlayProps) => {
+    let overlayClass: ArrayStringProps = ['overlay'];
+    if (variant === 'gradient-right') overlayClass.push('overlay--gradient-right');
+    if (variant === 'gradient-left') overlayClass.push('overlay--gradient-left');
+    if (variant === 'gradient-top') overlayClass.push('overlay--gradient-top');
+    if (variant === 'gradient-bottom') overlayClass.push('overlay--gradient-bottom');
+    overlayClass.push(`overlay--${opacity * 10}`);
+    overlayClass = joinArrayString(overlayClass);
+
+    const props: any = children.props;
+
+    let childClass: ArrayStringProps = [overlayClass];
+    if (props?.className) childClass.push(props.className);
+    childClass = joinArrayString(childClass);
+
+    return React.cloneElement(children as React.ReactElement<HTMLElement>, { className: childClass });
+};
+
+export default Overlay;
